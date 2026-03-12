cc84746ff2b4fa713d97acf1dc40654529a8ca1f
Angelo2463 <ampascual.student@ua.edu.ph>
2026-03-12 11:53:38 +0800

    Added Session14

---
+++ session14/index.js
@@ -0,0 +1,92 @@
+// This is how to print output using JS.
+// console.log(stringContent|VariableName);
+console.log("Hello world!");
+
+//To use comment we need to use -> // for single line comment
+// this is a single line comment
+
+/*
+this
+is
+a
+multi line
+comment
+*/
+
+// [Section] Variables
+// A variable is a container for storing data values. In JavaScript, we can declare a variable using the var, let, or const keyword.
+// The var keyword is used to declare a variable that can be re-assigned and has function scope.
+// The let keyword is used to declare a variable that can be re-assigned and has block scope.
+// The const keyword is used to declare a variable that cannot be re-assigned and has block scope.
+
+// [Section] Data Types
+// In JavaScript, there are several data types that we can use to store different kinds of values. The most common data types are:
+// 1. String: A sequence of characters enclosed in single or double quotes. Example: "Hello", 'World'
+// 2. Number: A numeric value that can be an integer or a floating-point number. Example: 42, 3.14
+    /*
+    let variable1 = "Hello"; // String
+    */
+// 3. Boolean: A logical value that can be either true or false. Example: true, false
+// 4. Null: A special value that represents the absence of any object value. Example: null
+// 5. Undefined: A special value that represents an uninitialized variable. Example: undefined
+// 6. Object: A collection of properties and methods. Example: { name: "John", age: 30 }
+// 7. Array: A special type of object that is used to store multiple values in a single variable. Example: [1, 2, 3, 4, 5]
+
+let age;
+let height = 180;
+
+console.log(height);
+console.log(height)
+console.log(        height          )
+console
+
+
+
+
+
+
+
+.log(height)
+
+// Sychronous PL -> JS
+
+let gwa = 99.75;
+
+console.log(gwa);
+
+// character Data types
+let firstName = "John";
+console.log(firstName);
+
+// typeof operator -> to check the data type of a variable
+
+console.log(typeof gwa);
+console.log(typeof firstName);
+console.log(typeof height);
+
+// Text Casing in JS
+// Camel Casing -> thisIsCamelCasing
+// Snake Casing -> this_is_snake_casing
+// Kebab Casing -> this-is-kebab-casing
+// Pascal Casing -> ThisIsPascalCasing
+// Pascal Kebab Casing -> This-Is-Kebab-Casing
+
+// Boolean Data Type
+
+let isRaining = true;
+let isMarried = false;
+
+console.log(isRaining);
+console.log(isMarried);
+console.log(typeof isRaining);
+console.log(typeof isMarried);
+
+// Concatenation -> the process of combining two or more strings together
+
+let firstName2 = "Jane";
+let lastName2 = "Doe"; 
+let fullName = firstName2 + " " + lastName2; // Concatenation using the + operator
+console.log(fullName);
+
+// Type Coercion -> the process of converting a value from one data type to another
+console.log("My overall grade is " + gwa); // Concatenation using the + operator
